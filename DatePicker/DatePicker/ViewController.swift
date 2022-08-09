44420aff9a77e69a77b469835f46629552ce5c54
dlwnsgurz <ljhlmkljhlmkljhlmk@naver.com>
2022-08-09 20:59:03 +0900

    DatePicker and Mission

---
+++ DatePicker/DatePicker/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  DatePicker
+//
+//  Created by LEE on 2022/08/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let timeSelector : Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    var alarmTime: String = ""
+    
+    @IBOutlet var currentTimeLabel: UILabel!
+    @IBOutlet var datePickerLabel: UILabel!
+    @IBOutlet var datePickerView: UIDatePicker!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+    }
+
+
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        
+        datePickerLabel.text = "선택시간 : " + formatter.string(from: sender.date)
+        
+        formatter.dateFormat = "hh:mm aaa"
+        alarmTime = formatter.string(from: datePickerView.date)
+        
+    }
+    @objc func updateTime(){
+        let date = NSDate()
+        
+        let formatter: DateFormatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        currentTimeLabel.text = "현재시각 : " + formatter.string(from: date as Date)
+        formatter.dateFormat = "hh:mm aaa"
+        let currentTime = formatter.string(from: date as Date)
+        if(currentTime == alarmTime)
+        {
+            view.backgroundColor = .red
+        }else{
+            view.backgroundColor = .white
+        }
+    }
+}
+
